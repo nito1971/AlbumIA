613d22d65732e1d859669b2edb24743af0a31e63
nito1971 <bgonpin@gmail.com>
2024-04-23 12:16:45 +0200

    Correcciones varias.

---
+++ inicioIdRuta.py
@@ -0,0 +1,157 @@
+from imageai.Detection import ObjectDetection
+import os
+import hashlib
+import pymongo
+
+# Declaramos variables.
+execution_path = os.getcwd()
+ruta = "/mnt/10.0.0.13/dos"
+rutaModelosIA = "/mnt/local/datos/ModelosIA"
+extensioneArchivosABuscar = ["jpg", "jpeg", "png", "pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt", "mp4", "avi", "mkv", "mov", "wmv", "flv", "mp4", "mp3"]
+
+# Se comprueba si la ruta que se le pasa como argumento
+# ya existe en la base de datos.
+def obtenerRutaArchvosMongoDb(ruta_archivo):
+    cliente = pymongo.MongoClient("mongodb://localhost:27017")
+    db = cliente["inventario"]
+    coleccion = db["archivos"]
+    documento = coleccion.find_one({"ruta_archivo": ruta_archivo})
+    return documento
+
+# Se inserta en la base de datos.
+def insert_mongo(_id, nombre_archivo, ruta_archivo, extensionArchivo, hash_archivo, peso):
+    try:
+      # Conexión a la base de datos local
+      cliente = pymongo.MongoClient("mongodb://localhost:27017")
+      # Seleccionar la base de datos y la colección
+      db = cliente["inventario"]
+      coleccion = db["archivos"]
+      # Datos a insertar
+      datos = [
+        {
+          "_id": _id,
+          "nombre_archivo": nombre_archivo,
+          "ruta_archivo": ruta_archivo,
+          "extensioArchivo": extensionArchivo,
+          "hash_archivo": hash_archivo, 
+          "peso": peso               
+        }      
+      ]
+
+      # Insertar los datos en la colección
+      coleccion.insert_many(datos)
+
+      # Cerrar la conexión a la base de datos
+      cliente.close()
+    except Exception as e:
+       print(f"Error al insertar en MongoDB: {e}")
+       cliente.close()
+       return False
+
+# Se genera un hash a partir de una cadena.
+def generarHashId(cadena):
+    return hashlib.sha512(cadena.encode()).hexdigest()
+
+# Se genera un hash a partir de un archivo.
+def generarHash(archivo):
+    try:
+        with open(archivo, 'rb') as f:
+            data = f.read()
+        return hashlib.sha512(data).hexdigest()
+    except Exception as e:
+        print(f"Error al generar el hash: {e}")
+        return None
+
+'''
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolov3.pt"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image1.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), minimum_percentage_probability=30)
+print(len(detections))
+
+for eachObject in detections:    
+    nombre = eachObject["name"]
+    probabilidad = eachObject["percentage_probability"]
+    coordenadas = eachObject["box_points"]
+    x1 = coordenadas[0]
+    y1 = coordenadas[1]
+    x2 = coordenadas[2]
+    y2 = coordenadas[3]
+    print("*" * 40)
+    print(f"Nombre objeto detectado: {nombre}")
+    print(f"Probalidas: {probabilidad}")
+    print(f"x1: {x1} y1: {y1} x2: {x2} y2: {y2}")
+
+'''
+# Se convierte de bytes a Gigabytes.
+def convertir_bytes_a_gb(bytes):
+    # 1 Gigabyte es igual a 1,073,741,824 bytes
+    gigabytes = bytes / 1073741824
+    return gigabytes
+
+# Se detectan los objetos en una imagen.
+def detectarObjetos(imagen):
+    salida = []
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(rutaModelosIA , "yolov3.pt"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , imagen), minimum_percentage_probability=30)
+    numeroObjetos = len(detections)
+    contador = 0
+    objeto = []
+    for eachObject in detections:        
+        contador += 1    
+        nombre = eachObject["name"]
+        probabilidad = eachObject["percentage_probability"]
+        coordenadas = eachObject["box_points"]
+        x1, y1, x2, y2 = coordenadas
+        objeto = [contador, nombre, probabilidad, coordenadas]        
+        salida.append(objeto)
+        objeto = []              
+    return salida
+
+
+
+'''
+os.system("clear")
+objetosDetectados = detectarObjetos("image1.jpg")
+for objetos in objetosDetectados:
+    print(objetos)
+'''
+
+
+# Se procesa un archivo.
+def process_file(file_path):
+    try:        
+        
+        if obtenerRutaArchvosMongoDb(file_path) != file_path:            
+            file_name = os.path.basename(file_path) 
+            extension = file_name.split(".")[-1]       
+            size = convertir_bytes_a_gb(os.path.getsize(file_path))
+            if extension in extensioneArchivosABuscar:                
+                if size < 1024*3:
+                    _id = file_path
+                    hash_archivo = generarHashId(file_path)
+                    insert_mongo(_id, file_name, file_path, extension, hash_archivo, size)
+                    print(f"File: {file_name} - Size: {size} GB")            
+                else:
+                    _id = file_path
+                    hash_archivo = None
+                    insert_mongo(_id, file_name, file_path, extension, hash_archivo, size)
+                    print(f"File: {file_name} - Size: {size} GB")
+            else:
+                print(f"El archivo {file_name} no es un archivo válido!")
+        else:
+            print(f"El archivo {file_path} existe!")
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        pass
+
+# Se ejecuta el programa.
+if __name__ == "__main__":
+    for root, dirs, files in os.walk(ruta):
+        for file in files:
+            process_file(os.path.join(root, file))
